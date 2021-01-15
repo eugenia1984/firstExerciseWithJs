b49c8f637bfd20365df569a7bf4357928c289210
Eugenia <costamariaeugenia1@gmail.com>
2021-01-15 20:59:17 -0300

    add files via upload
    I added the files

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+
+var ingrediente1 = prompt('ingrese un primer ingrediente para una receta')
+var ingrediente2 = prompt('ingrese un segundo ingrediente para la misme receta')
+var ingrediente3 = prompt('ingrese un tercer ingrediente para la misma receta')
+
+alert ('Usted eligió:' + ' ' + ingrediente1 + ' ' + ingrediente2 + ' ' + ingrediente3)
+
+
